f9f9054ba60301452eff05fd2cea0ab3d5fb4c9f
Josh Paul <josh.paul@nutanix.com>
2018-12-20 09:46:17 -0800

    Add handling for iter objects to list

--- tests/test_dotteddict.py
+++ tests/test_dotteddict.py
@@ -4,52 +4,53 @@ from dotted_dict import DottedDict
 
 
 class dotteddictTests(unittest.TestCase):
-    '''
+    """
     Test cases for the ``dotteddict`` class.
-    '''
+    """
+
     def test_child_dictionary_types(self):
-        dotted = DottedDict({'x': {'y': {'z': 5}}})
+        dotted = DottedDict({"x": {"y": {"z": 5}}})
 
         self.assertEqual(type(dotted.x), DottedDict)
         self.assertEqual(type(dotted.x.y), DottedDict)
 
     def test_child_dictionary_types_accessor(self):
-        dotted = DottedDict({'x': {'y': {'z': 5}}})
+        dotted = DottedDict({"x": {"y": {"z": 5}}})
 
-        self.assertEqual(type(dotted['x']), DottedDict)
-        self.assertEqual(type(dotted['x']['y']), DottedDict)
+        self.assertEqual(type(dotted["x"]), DottedDict)
+        self.assertEqual(type(dotted["x"]["y"]), DottedDict)
 
     def test_children_equivalence(self):
-        dotted = DottedDict({'x': {'y': {'z': 5}}})
+        dotted = DottedDict({"x": {"y": {"z": 5}}})
 
-        self.assertTrue(dotted.x is dotted['x'])
-        self.assertTrue(dotted.x.y is dotted['x']['y'])
+        self.assertTrue(dotted.x is dotted["x"])
+        self.assertTrue(dotted.x.y is dotted["x"]["y"])
 
     def test_construction_without_data(self):
         dotted = DottedDict()
         self.assertTrue(isinstance(dotted, DottedDict))
 
     def test_copy(self):
-        my_dict = DottedDict({'my key two': {'b': 'c', 1: 2}})
+        my_dict = DottedDict({"my key two": {"b": "c", 1: 2}})
         dotted = my_dict.copy()
         self.assertEquals(my_dict, dotted)
 
     def test_delete(self):
-        dotted = DottedDict({'foo': 1})
+        dotted = DottedDict({"foo": 1})
         del dotted.foo
 
         self.assertRaises(KeyError)
 
     def test_dotted_get(self):
-        value = 'foo'
-        data = {'color': {'blue': value}}
+        value = "foo"
+        data = {"color": {"blue": value}}
 
         dotted = DottedDict(data)
 
-        self.assertEqual(dotted.get('color').get('blue'), value)
+        self.assertEqual(dotted.get("color").get("blue"), value)
 
     def test_items_format_invocation(self):
-        items = [('x', 1), ('y', 2), ('z', 3)]
+        items = [("x", 1), ("y", 2), ("z", 3)]
         dotted = DottedDict(items)
 
         self.assertEquals(dotted.x, 1)
@@ -57,28 +58,28 @@ class dotteddictTests(unittest.TestCase):
         self.assertEquals(dotted.z, 3)
 
     def test_missing_attribute_error(self):
-        dotted = DottedDict({'foo': None})
+        dotted = DottedDict({"foo": None})
 
         with self.assertRaises(AttributeError):
             dotted.missing
 
     def test_none_value(self):
         value = None
-        dotted = DottedDict({'foo': value})
+        dotted = DottedDict({"foo": value})
         self.assertTrue(dotted.foo is value)
 
     def test_not_valid_identifier(self):
-        dotted = DottedDict({'foo-bar': 1, '.foo': 1, 'foo baz': 1, 1: 2})
+        dotted = DottedDict({"foo-bar": 1, ".foo": 1, "foo baz": 1, 1: 2})
         self.assertTrue(dotted.foo_bar, 1)
         self.assertTrue(dotted._foo, 1)
         self.assertTrue(dotted.foo_baz, 1)
         self.assertTrue(dotted._1, 2)
         # Reserved keyword
         with self.assertRaises(ValueError):
-            DottedDict({'lambda': 1})
+            DottedDict({"lambda": 1})
 
     def test_repr(self):
-        dotted = DottedDict({'foo-bar': 1, '.foo': 1, 'foo baz': 1, 1: 2})
+        dotted = DottedDict({"foo-bar": 1, ".foo": 1, "foo baz": 1, 1: 2})
         assert eval(repr(dotted)) == dotted
 
     def test_set_via_attrib(self):
@@ -86,10 +87,10 @@ class dotteddictTests(unittest.TestCase):
         meaning = DottedDict()
         meaning.oflife = value
 
-        self.assertEqual(meaning['oflife'], value)
+        self.assertEqual(meaning["oflife"], value)
 
     def test_setting_and_accessing(self):
-        key = 'x'
+        key = "x"
         value = 5
         dotted = DottedDict({key: value})
 
@@ -98,10 +99,10 @@ class dotteddictTests(unittest.TestCase):
         self.assertEqual(dotted.x, dotted[key])
 
     def test_to_dict(self):
-        dotted = DottedDict(DottedDict({'a': 'b', 'c': {'d': 'e'}, 'f': [{'g': 'h'}, 1]}))
+        dotted = DottedDict(DottedDict({"a": "b", "c": {"d": "e"}, "f": [{"g": "h"}, 1]}))
         my_dict = dotted.to_dict()
         self.assertEquals(dotted, my_dict)
         self.assertEqual(type(dotted), DottedDict)
         self.assertEqual(type(dotted.f[0]), DottedDict)
         self.assertEqual(type(my_dict), dict)
-        self.assertEqual(type(my_dict['f'][0]), dict)
+        self.assertEqual(type(my_dict["f"][0]), dict)
