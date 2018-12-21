6e6c0a913257dfbc65c80c4ecc1c8e64b0aad220
Will <1486507+graywizardx@users.noreply.github.com>
2018-12-20 20:36:46 -0800

    Update __init__.py

--- dotted_dict/__init__.py
+++ dotted_dict/__init__.py
@@ -27,7 +27,7 @@ class DottedDict(dict):
         self.__delitem__(item)
 
     def __delitem__(self, key):
-        super(DottedDict, self).__delitem__(key)
+        super(self.__class__, self).__delitem__(key)
         del self.__dict__[key]
 
     def __getattr__(self, attr):
@@ -44,7 +44,7 @@ class DottedDict(dict):
         """
         Wrap the returned dict in DottedDict() on output.
         """
-        return '{0}({1})'.format(type(self).__name__, super(DottedDict, self).__repr__())
+        return '{0}({1})'.format(type(self).__name__, super(self.__class__, self).__repr__())
 
     def __setattr__(self, key, value):
         # No need to run _is_valid_identifier since a syntax error is raised if invalid attr name
@@ -58,7 +58,7 @@ class DottedDict(dict):
                 key = self._make_safe_(key)
             else:
                 raise ValueError('Key "{0}" is a reserved keyword.'.format(key))
-        super(DottedDict, self).__setitem__(key, value)
+        super(self.__class__, self).__setitem__(key, value)
         self.__dict__.update({key: value})
 
     def _is_valid_identifier_(self, identifier):
@@ -103,12 +103,12 @@ class DottedDict(dict):
         """
         for key, value in input_item.items():
             if isinstance(value, dict):
-                value = DottedDict(**{str(k): v for k, v in value.items()})
+                value = self.__class__(**{str(k): v for k, v in value.items()})
             if isinstance(value, list):
                 _list = []
                 for item in value:
                     if isinstance(item, dict):
-                        _list.append(DottedDict(item))
+                        _list.append(self.__class__(item))
                     else:
                         _list.append(item)
                 value = _list
@@ -142,5 +142,129 @@ class DottedDict(dict):
 
 
 class PreserveKeysDottedDict(DottedDict):
+    """
+    Override for the dict object to allow referencing of keys as attributes, i.e. dict.key
+    """
+
+    def __init__(self, *args, **kwargs):
+        for arg in args:
+            if isinstance(arg, dict):
+                self._parse_input_(arg)
+            elif isinstance(arg, list):
+                for k, v in arg:
+                    self.__setitem__(k, v)
+            elif hasattr(arg, "__iter__"):
+                for k, v in list(arg):
+                    self.__setitem__(k, v)
+
+        if kwargs:
+            self._parse_input_(kwargs)
+
+    def __delattr__(self, item):
+        self.__delitem__(item)
+
+    def __delitem__(self, key):
+        super(self.__class__, self).__delitem__(key)
+        del self.__dict__[key]
+
+    def __getattr__(self, attr):
+        try:
+            return self.__dict__[attr]
+        # Do this to match python default behavior
+        except KeyError:
+            raise AttributeError(attr)
+
+    def __getitem__(self, key):
+        return self.__dict__[key]
+
+    def __repr__(self):
+        """
+        Wrap the returned dict in DottedDict() on output.
+        """
+        return '{0}({1})'.format(type(self).__name__, super(self.__class__, self).__repr__())
+
+    def __setattr__(self, key, value):
+        # No need to run _is_valid_identifier since a syntax error is raised if invalid attr name
+        self.__setitem__(key, value)
+
+    def __setitem__(self, key, value):
+        try:
+            self._is_valid_identifier_(key)
+        except ValueError:
+            if not keyword.iskeyword(key):
+                key = self._make_safe_(key)
+            else:
+                raise ValueError('Key "{0}" is a reserved keyword.'.format(key))
+        super(self.__class__, self).__setitem__(key, value)
+        self.__dict__.update({key: value})
+
     def _is_valid_identifier_(self, identifier):
+        """ Determine if key is a valid identifier. """
         return True
+
+    def _make_safe_(self, key):
+        """
+        Replace the space characters on the key with _ to make valid attrs.
+        """
+        key = str(key)
+        allowed = string.ascii_letters + string.digits + "_"
+        # Replace spaces with _
+        if " " in key:
+            key = key.replace(" ", "_")
+        # Find invalid characters for use of key as attr
+        diff = set(key).difference(set(allowed))
+        # Replace invalid characters with _
+        if diff:
+            for char in diff:
+                key = key.replace(char, "_")
+        # Add _ if key begins with int
+        try:
+            int(key[0])
+        except ValueError:
+            pass
+        else:
+            key = "_{0}".format(key)
+        return key
+
+    def _parse_input_(self, input_item):
+        """
+        Parse the input item if dict into the dotted_dict constructor.
+        """
+        for key, value in input_item.items():
+            if isinstance(value, dict):
+                value = self.__class__(**{str(k): v for k, v in value.items()})
+            if isinstance(value, list):
+                _list = []
+                for item in value:
+                    if isinstance(item, dict):
+                        _list.append(self.__class__(item))
+                    else:
+                        _list.append(item)
+                value = _list
+            self.__setitem__(key, value)
+
+    def copy(self):
+        """
+        Ensure copy object is DottedDict, not dict.
+        """
+        return type(self)(self)
+
+    def to_dict(self):
+        """
+        Recursive conversion back to dict.
+        """
+        out = dict(self)
+        for key, value in out.items():
+            if value is self:
+                out[key] = out
+            elif hasattr(value, "to_dict"):
+                out[key] = value.to_dict()
+            elif isinstance(value, list):
+                _list = []
+                for item in value:
+                    if hasattr(item, "to_dict"):
+                        _list.append(item.to_dict())
+                    else:
+                        _list.append(item)
+                out[key] = _list
+        return out
